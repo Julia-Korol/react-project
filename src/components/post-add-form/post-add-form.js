711eb0bbe06970a327e4a0949496415a85a7aa3d
Julia-Korol <korol.julia@inbox.ru>
2021-09-26 15:05:41 +0300

    project

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './post-add-form.css';
+
+const PostAddForm = ({ onAdd }) => {
+  return (
+    <div className="bottom-panel f-flex">
+      <input
+        type="text"
+        placeholder="О чем вы думаете сейчас"
+        className="form-control new-post-label"
+      />
+      <button type="submit" className="btn btn-outline-secondary" onClick={() => onAdd('Hello')}>
+        Добавить
+      </button>
+    </div>
+  );
+};
+
+export default PostAddForm;
